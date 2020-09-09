eca9ccfc18dc113625dea04acb0eafc6d210e055
Stephen Huisinga <stephenhuisinga@gmail.com>
2020-09-08 22:11:51 -0500

    mvp complete

--- src/App.js
+++ src/App.js
@@ -1,27 +1,71 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
 import logo from './logo.svg';
-import axios from 'axios'
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    gitHubProfile: false,
+    profileSearch: "",
+    error: "",
+  };
+
+  handleChanges = (e) => {
+    this.setState({
+      profileSearch: e.target.value,
+    });
+  };
+
+  fetchProfiles = (e) => {
+    e.preventDefault();
+    axios
+      .get(`https://api.github.com/users/${this.state.profileSearch}`)
+      .then((response) => {
+        this.setState({
+          gitHubProfile: response.data,
+          error: "",
+        });
+      })
+      .catch((err) => {
+        this.setState({
+          error:
+            "No profile was found",
+        });
+      });
+  };
+
+  render() {
+    console.log(this.state);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>GitHub Profile Finder</h1>
+          <img src={logo} className="App-logo" alt="logo" />
+          <input
+            type="text"
+            value={this.state.profileSearch}
+            onChange={this.handleChanges}
+          />
+          <button onClick={this.fetchProfiles}>Search</button>
+          {this.state.error && (
+            <p style={{ color: "red" }}>{this.state.error}</p>
+          )}
+          {this.state.gitHubProfile && (
+            <p style={{ color: "red" }}>{this.state.gitHubProfile.username}</p>
+          )}
+        </header>
+        <div className="profiles">
+          {this.state.gitHubProfile.id && (
+            <div>
+              <h2>{this.state.gitHubProfile.name}</h2>
+              <h3>{this.state.gitHubProfile.login} | {this.state.gitHubProfile.location}</h3>
+              <h3>{this.state.gitHubProfile.followers} Followers | Following {this.state.gitHubProfile.following}</h3>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
